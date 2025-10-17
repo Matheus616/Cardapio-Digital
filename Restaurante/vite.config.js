80a11fe7f2f81fbb5898990ce88bbe37b29f00aa
Matheus616 <matheussc616@gmail.com>
2025-10-16 22:00:30 -0300

    Config acesso externo

--- Restaurante/vite.config.js
+++ Restaurante/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // permite acesso externo
+    port: 5173, // pode mudar a porta se quiser
+    allowedHosts: ['sonly-foreseeable-cathy.ngrok-free.dev'], // 👈 adiciona o domínio do ngrok
+  },
 })
+
